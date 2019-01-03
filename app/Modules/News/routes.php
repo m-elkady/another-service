46ef886840413709f721d4a532e88d0950fb6690
m-elkady <m.elkady365@gmail.com>
2019-01-03 09:23:42 +0200

    Update routes.php

--- app/Modules/News/routes.php
+++ app/Modules/News/routes.php
@@ -1,15 +1,9 @@
 <?php
 
 $router->group(['namespace' => 'App\Modules\News\Controllers','prefix'=>'news'], function ($router) {
-    $router->get('/', 'NewsController@index');
-    $router->post('/', 'NewsController@index');
-    $router->get('/{action:add|create|insert}', 'NewsController@add');
-    $router->post('/{action:add|create|insert}', 'NewsController@add');
-    $router->get('/{action:update|change|rename|modify}', 'NewsController@edit');
-    $router->post('/{action:update|change|rename|modify}', 'NewsController@edit');
-    $router->get('/{action:delete|remove|del|destroy}', 'NewsController@delete');
-    $router->post('/{action:delete|remove|del|destroy}', 'NewsController@delete');
-    $router->delete('/{action:delete|remove|del|destroy}', 'NewsController@delete');
-    $router->get('/{action:select|list|view|search}', 'NewsController@view');
-    $router->post('/{action:select|list|view|search}', 'NewsController@view');
+    $router->any('/', 'NewsController@index');
+    $router->any('/{action:add|create|insert}', 'NewsController@add');
+    $router->any('/{action:update|change|rename|modify}', 'NewsController@edit');
+    $router->any('/{action:delete|remove|del|destroy}', 'NewsController@delete');
+    $router->any('/{action:select|list|view|search}', 'NewsController@view');
 });
